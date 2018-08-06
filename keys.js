5a2121f0cbcb38fa974d05f0d18fa2f4e521d8e3
Bryan C <bryan.carvalho.design@gmail.com>
2018-08-06 14:08:36 -0400

    updates

--- keys.js
+++ keys.js
@@ -10,3 +10,4 @@ exports.twitter = {
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,81 @@
 require("dotenv").config();
+
+console.log("hello");
+
+let keys = require('./keys');
+let fs = require('fs');
+let request = require(`request`);
+let Spotify = require('node-spotify-api');
+
+let command = process.argv[2];
+let content = process.argv[3];
+let spotify = new Spotify(keys.spotify);
+
+if (command === "spotify-this-song") {
+    spotifyRun();
+}
+
+else if (command === "movie-this") {
+    omdbRun();
+}
+
+else if (command === "do-what-it-says") {
+    whatItSays();
+};
+
+function spotifyRun() {
+    if (!content) {
+        content = "The Sign";
+    }
+
+    spotify.search({ type: "track", query: content, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+
+        data = JSON.parse(JSON.stringify(data));
+
+        console.log(
+            `Artists: ${data.tracks.items[0].album.artists[0].name}`,
+            `\nTrack: ${data.tracks.items[0].name}`,
+            `\nPreview: ${data.tracks.items[0].preview_url}`,
+            `\nAlbum: ${data.tracks.items[0].album.name}`
+        );
+    });
+};
+
+function omdbRun() {
+    if (!content) {
+        content = "Mr.Nobody";
+    }
+
+    let omdb = `http://www.omdbapi.com/?t="${content}"&y=&plot=short&apikey=trilogy`;
+
+    request(omdb, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            body = JSON.parse(body, null, 2);
+            console.log(
+                `Title: ${body.Title}`,
+                `\nYear: ${body.Year}`,
+                `\nIMDB Rating: ${body.imdbRating}`,
+                `\n${body.Ratings[1].Source} Rating: ${body.Ratings[1].Value}`,
+                `\nCountry Produced: ${body.Country}`,
+                `\nLanguage: ${body.Language}`,
+                `\nPlot: ${body.Plot}`,
+                `\nActors: ${body.Actors}`
+            );
+        }
+    });
+}
+
+function whatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+
+        data = data.split(",");
+        command = data[0];
+        content = data[1];
+    });
+}
